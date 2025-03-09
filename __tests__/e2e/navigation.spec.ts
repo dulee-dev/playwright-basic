2f1b0507b3732ac15c0996b9019064d4293d897f
dulee <dulee.dev@gmail.com>
2025-03-09 15:41:01 +0900

    FEAT: ch 8 done

--- __tests__/e2e/navigation.spec.ts
+++ __tests__/e2e/navigation.spec.ts
@@ -10,6 +10,28 @@ test('if user visit home and click "Get Started", h1 "Introduction" is visible a
   await page.goto(startUrl);
   await page.getByRole('link', { name: 'Get Started' }).click();
 
+  // console.log(
+  //   await page.getByRole('heading', { name: h1, level: 1 }).isVisible()
+  // );
+  // await page.waitForTimeout(30);
+  // console.log(
+  //   await page.getByRole('heading', { name: h1, level: 1 }).isVisible()
+  // );
+  // await page.waitForTimeout(30);
+  // console.log(
+  //   await page.getByRole('heading', { name: h1, level: 1 }).isVisible()
+  // );
+  // await page.waitForTimeout(30);
+  // console.log(
+  //   await page.getByRole('heading', { name: h1, level: 1 }).isVisible()
+  // );
+
+  // await page.waitForTimeout(20);
+  // const isVisible = await page
+  //   .getByRole('heading', { name: h1, level: 1 })
+  //   .isVisible();
+  // expect(isVisible).toEqual(true);
+
   await expect(page.getByRole('heading', { name: h1, level: 1 })).toBeVisible();
   await expect(page).toHaveTitle(title);
 });
